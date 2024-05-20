34028ec4f5d2fe93a1c1686614fdacdd757aba37
Nithin J Dennis <nithinjdennis@Nithins-MacBook-Pro.local>
2024-05-20 17:27:11 +0530

    this is corrected database

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(database=['default'])
+                self.check(databases=['default'])
                 db_up= True
             except(Psycopg20pError,OperationalError):
                 self.stdout.write("delay for a second")
